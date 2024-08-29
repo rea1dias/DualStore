0a0b6d6d0cd5be78cccc901d238d0a074116855e
Dias Rakhman <rahmanovdias13@gmail.com>
2024-08-29 18:04:18 +0500

    First commit for this project

---
+++ src/main/java/com/example/DualStore/DualStoreApplication.java
@@ -0,0 +1,15 @@
+package com.example.DualStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DualStoreApplication {
+
+	public static void main(String[] args) {
+
+
+		SpringApplication.run(DualStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DualStore/DualStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DualStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DualStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
