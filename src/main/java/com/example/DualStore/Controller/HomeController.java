e69879cdf00e1ffb1af7e493fec7f3488834aca1
Dias Rakhman <rahmanovdias13@gmail.com>
2024-09-01 22:50:37 +0500

    - A new page for viewing the product has been added. (Website layout only)

--- src/main/java/com/example/DualStore/Controller/HomeController.java
+++ src/main/java/com/example/DualStore/Controller/HomeController.java
@@ -24,6 +24,19 @@ public class HomeController {
         return "register";
     }
 
+    @GetMapping("/products")
+    public String products()
+    {
+        return "product";
+    }
+
+    @GetMapping("/product")
+    public String product()
+    {
+        return "view_product";
+    }
+
+
 
 
 
